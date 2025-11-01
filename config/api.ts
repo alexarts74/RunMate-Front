32daa31909f0b84f5309657e1e4d37a8522f2f85
alexandre artus <artusalexandre74@gmail.com>
2025-11-01 16:37:49 +0100

    wip

--- config/api.ts
+++ config/api.ts
@@ -2,8 +2,8 @@ import Constants from "expo-constants";
 
 // URL par défaut pour le développement local
 // Note: localhost ne fonctionne pas sur téléphone physique
-// L'IP locale sera détectée automatiquement par app.config.js
-const DEFAULT_API_URL = "http://192.168.1.206:3000";
+// L'IP locale sera détectée automatiquement par app.config.js ou .env
+const DEFAULT_API_URL = "http://localhost:3000";
 
 export const API_CONFIG = {
   BASE_URL: Constants.expoConfig?.extra?.apiBaseUrl || DEFAULT_API_URL,
